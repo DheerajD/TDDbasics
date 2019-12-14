79966f437301004af107b6ec6c267a5ee6dca377
DheerajD <dheeraj.doodhya@gmail.com>
2019-12-14 21:05:52 +0530

    Testcase for adding 2 numbers

---
+++ src/test/java/io/dheerajd/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package io.dheerajd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void testAdd() {
+		MathUtils mathUtils = new MathUtils();
+		int expected = 2;
+		int actual = mathUtils.add(1, 1);
+		assertEquals(expected, actual);
+	}
+
+}
